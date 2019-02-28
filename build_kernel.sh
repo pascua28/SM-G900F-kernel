823c8e23243581e5dab00617004a3abdf062912b
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-02-28 20:15:39 +0800

    fix build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,3 +9,5 @@ make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_k
 make -j64 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+
+tools/dtbTool -2 -o output/arch/arm/boot/dt.img -s 2048 -p output/scripts/dtc/ output/arch/arm/boot/
